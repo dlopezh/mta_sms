5e5ec33ac691b13e5038511a40d38c2058ae5386
dlopez <daniellopezh@gmail.com>
2014-01-13 14:07:25 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MtaSms::Application.config.secret_token = 'a799ec5a4428e6876a4e44896a89b9c2d6945fa0e03f8dd1f9dd890f32290fd2ce0ea15b6539fb51355199847750115aa1f1f4e7976faefe2b8490d6ccf208bc'
