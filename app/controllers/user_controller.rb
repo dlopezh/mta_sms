37a29dc83b866bcf7b218142f84aa4a691229e0e
dlopez <daniellopezh@gmail.com>
2014-01-17 00:24:22 -0500

    sms working from form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -21,6 +21,12 @@ end
 	@user.save
 	@user.line = line
 	@user.save
+	url = "https://api.sendhub.com/v1/messages/?username=9177559495\&api_key=5d04b7c26946c768565d4c10b00c11f58ac526a6"
+        @response= HTTParty.post(url, 
+        :body => { :contacts => ['+1'+@user.mobile.to_s],
+                    :text => @user.line.name + ' - ' + @user.line.status + ' - ' + @user.line.description 
+             }.to_json,
+    :headers => { 'Content-Type' => 'application/json' } )
 	redirect_to '/'
 	end
 
